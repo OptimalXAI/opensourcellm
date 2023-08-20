de349f7a48702f7044a10e162ce1dc3a79e199d7
OptimalXAI <119094976+OptimalXAI@users.noreply.github.com>
2023-08-20 08:22:12 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,28 @@
-from app import multiple
+import requests
 
-output = multiple(5,6)
+# Base URL of the API
+base_url = 'http://192.168.1.45:5000'  # Replace with the actual URL where your API is running
 
-assert output == 30
+# Test data
+user_data = {
+    "username": "optimalxai user1",
+    "email": "optimalxaiuser1@example.com"
+}
+
+# Test the create_user endpoint
+def test_create_user():
+    endpoint = '/user'
+    url = base_url + endpoint
+
+    response = requests.post(url, json=user_data)
+    print(response.json())
+    # Check if the response status code is 201 (Created)
+    assert response.status_code == 201
+    
+    # Check if the response data matches the test data
+    assert response.json() == user_data
+    
+    print(f"Test for {endpoint} passed!")
+
+if __name__ == '__main__':
+    test_create_user()
