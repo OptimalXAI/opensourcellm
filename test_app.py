e2aa27d23d456a9ccff255c331e96f7fe37cc054
OptimalXAI <119094976+OptimalXAI@users.noreply.github.com>
2023-08-20 10:10:27 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,28 +1,6 @@
-import requests
+from app import ci_func
 
-# Base URL of the API
-base_url = 'http://127.0.0.1:5000'  # Replace with the actual URL where your API is running
+sum = ci_func(1,2)
 
-# Test data
-user_data = {
-    "username": "optimalxai user1",
-    "email": "optimalxaiuser1@example.com"
-}
-
-# Test the create_user endpoint
-def test_create_user():
-    endpoint = '/user'
-    url = base_url + endpoint
-
-    response = requests.post(url, json=user_data)
-    print(response.json())
-    # Check if the response status code is 201 (Created)
-    assert response.status_code == 201
-    
-    # Check if the response data matches the test data
-    assert response.json() == user_data
-    
-    print(f"Test for {endpoint} passed!")
-
-if __name__ == '__main__':
-    test_create_user()
+assert sum == 3
+print(sum)
