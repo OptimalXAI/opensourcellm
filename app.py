d2e58075de30618d248d71419cc27f57ee2fb6ca
OptimalXAI <119094976+OptimalXAI@users.noreply.github.com>
2023-08-20 10:10:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,2 @@
-from flask import Flask, request, jsonify
-
-
-app = Flask(__name__)
-
-
-# Create (POST) operation
-@app.route('/user', methods=['POST'])
-def create_user():
-    data = request.get_json()  
-    
-    return jsonify(data), 201
-
-# Run the Flask app
-if __name__ == '__main__':    
-    app.run(debug = True)
+def ci_func(arg_1, arg_2):
+    return arg_1+arg_2
